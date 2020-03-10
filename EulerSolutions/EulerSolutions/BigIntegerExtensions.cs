38cc5eba2fb048023e7ed2f07a6d98561472a3ec
Adam Drewery <adam.drewery@live.co.uk>
2020-03-10 22:13:12 +0000

    Tidy up extension methods a little.

--- EulerSolutions/EulerSolutions/BigIntegerExtensions.cs
+++ EulerSolutions/EulerSolutions/BigIntegerExtensions.cs
@@ -7,16 +7,21 @@ namespace EulerSolutions
 	{
 		public static IEnumerable<int> Digits(this BigInteger input)
 		{
-			if (input == 0) yield return 0;
+			if (input == 0) return new[] { 0 };
+		
+			var digits = new List<int>();
 			
 			// I did google this one:
 			// https://www.includehelp.com/dot-net/find-sum-of-all-digits-of-a-given-number.aspx
 			// Without googling a solution I'd have just converted the source to a string and parsed each digit.
 			while (input != 0)
 			{
-				yield return (short) (input % 10);
+				digits.Add((int) (input % 10));
 				input /= 10;
 			}
+
+			digits.Reverse();
+			return digits;
 		}
 	}
 }
--- EulerSolutions/EulerSolutions/Int64Extensions.cs
+++ EulerSolutions/EulerSolutions/Int64Extensions.cs
@@ -6,10 +6,10 @@ namespace EulerSolutions
 	{
 		public static IEnumerable<int> Digits(this long input)
 		{
+			if (input == 0) return new[] { 0 };
+			
 			var digits = new List<int>();
-			// I googled this:
-			// https://www.includehelp.com/dot-net/find-sum-of-all-digits-of-a-given-number.aspx
-			// Without googling a solution I'd have just converted the source to a string and parsed each digit.
+			
 			while (input != 0)
 			{
 				digits.Add((int) (input % 10));
